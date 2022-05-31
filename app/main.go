b450cb3081c3e44d070a64af4c983747914c8a32
LAIS CARVALHO DE LIMA <lais.carvalho@g.globo>
2022-05-30 21:01:16 -0300

    Update Readme, fix docker-compose and add proxy cache

--- app/main.go
+++ app/main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("APP:", os.Getenv("APP"))
+		appName := os.Getenv("APP")
 		w.WriteHeader(http.StatusOK)
-		io.WriteString(w, "Inicio\n")
+		io.WriteString(w, "Inicio"+appName)
+	})
+
+	http.HandleFunc("/user", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("APP:", os.Getenv("APP"))
+		appName := os.Getenv("APP")
+		w.WriteHeader(http.StatusOK)
+		userName := r.URL.Query().Get("name")
+		if userName != "" {
+			io.WriteString(w, appName+":Username is"+userName)
+		}
 	})
 
 	err := http.ListenAndServe(":8081", nil)
